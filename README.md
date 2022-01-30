d17d1943f0ae1b4bad3242a953afa2ed9ceba814
ambiti0n <fabulousfresh010@gmail.com>
2022-01-30 13:40:12 +0100

    crypto-dev ico

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ICO-Initial-Coin-Offering-
+
+Crypto Devs Token Contract Address: 0x5F6dB69D8648A22929a87AF11eC9f1bF535beB86
+
---
+++ hardhat-project/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x67CD9399ef6Ca05c579C2B9beF42b64ed1Fd5883";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
